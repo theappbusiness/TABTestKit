21223f80b9ef0ec7e01cecadc0fcc75ca8c491bc
Zachary Borrelli <zac@theappbusiness.com>
2017-09-14 14:55:36 +0100

    Installed XCTest-Gherkin

--- Example/Pods/Target Support Files/Pods-TABTestKit_Example/Pods-TABTestKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-TABTestKit_Example/Pods-TABTestKit_Example-frameworks.sh
@@ -103,9 +103,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/TABTestKit/TABTestKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/XCTest-Gherkin/XCTest_Gherkin.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/TABTestKit/TABTestKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/XCTest-Gherkin/XCTest_Gherkin.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
---
+++ Example/Pods/XCTest-Gherkin/Pod/Core/StringGherkinExtension.swift
@@ -0,0 +1,60 @@
+//
+//  ScreenHelperExtensions.swift
+//  whats-new
+//
+//  Created by Sam Dean on 03/11/2015.
+//  Copyright © 2015 net-a-porter. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    
+    /**
+     Given a human readable string this method will return a 'CamelCaseified' version of it.
+
+     - `"product details".screenName` returns `"ProductDetails"`
+
+    */
+    var camelCaseify: String {
+        get {
+            guard case let characters = (self.characters.split { $0 == " " || $0 == "-" }), characters.count > 1 else {
+                return self.uppercaseFirstLetterString
+            }
+            return characters.map { String($0).lowercased().uppercaseFirstLetterString }.joined(separator: "")
+        }
+    }
+
+    /**
+     The reciever with the first letter uppercased - the rest of the string remains untouched.
+     
+     - "hello" -> "Hello"
+     - "HeLLO" -> "HeLLO"
+    */
+    var uppercaseFirstLetterString: String {
+        get {
+            guard case let characters = self.characters, let firstCharacter = characters.first else { return self }
+            return String(firstCharacter).uppercased() + String(characters.dropFirst())
+        }
+    }
+    
+    /**
+     Given `CamelCaseString` this will return `Camel Case String`
+     
+     TODO: There is probably a more efficient way to do this. Technically this is O(n) I guess, just not a very nice O(n).
+     */
+    var humanReadableString: String {
+        get {
+            guard case let characters = self.characters, characters.count > 1, let firstCharacter = characters.first else { return self }
+            return String(firstCharacter) + characters.dropFirst().reduce("") { (word, character) in
+                let letter = String(character)
+                if letter == letter.uppercased() {
+                    return word + " " + letter
+                }
+                else {
+                    return word + letter
+                }
+            }
+        }
+    }
+}
