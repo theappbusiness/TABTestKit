5316ce57a9eb4de39bb6038f9363e4d21c86bb6c
Zachary Borrelli <zac@theappbusiness.com>
2017-09-14 15:23:34 +0100

    Create UI tests scheme

---
+++ Example/TABTestKit_ExampleUITests/ExampleFeature.swift
@@ -0,0 +1,13 @@
+//
+//  ExampleFeature.swift
+//  TABTestKit
+//
+//  Created by Zachary Borrelli on 14/09/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class ExampleFeature: Feature {
+  
+}
