dc33d0e11f2c2a7d9f1ea1ac1243472538c0f453
Suyash Srijan <suyashsrijan@outlook.com>
2018-11-02 00:05:33 +0000

    Updates copyright to TAB

--- TABTestKit/Classes/Biometrics/Biometrics.m
+++ TABTestKit/Classes/Biometrics/Biometrics.m
@@ -3,7 +3,7 @@
 //  TABTestKit
 //
 //  Created by Kane Cheshire on 31/10/2018.
-//  Copyright © 2018 kane.codes. All rights reserved.
+//  Copyright © 2018 The App Business. All rights reserved.
 //
 
 #import "Biometrics.h"
