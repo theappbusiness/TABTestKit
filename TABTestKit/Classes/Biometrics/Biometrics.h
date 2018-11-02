68c17d4777ebbf5b136bdaa85c1ad4aa6f4e0e5a
Suyash Srijan <suyashsrijan@outlook.com>
2018-11-02 00:16:06 +0000

    Adds basic docs to the header

--- TABTestKit/Classes/Biometrics/Biometrics.h
+++ TABTestKit/Classes/Biometrics/Biometrics.h
@@ -12,9 +12,30 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Biometrics : NSObject
 
+/*!
+ * Enroll biometrics on the simulator
+ */
 + (void)enrolled;
+
+/*!
+ * Unenroll biometrics on the simulator
+ */
 + (void)unenrolled;
+
+/*!
+ * Performs a successful biometric authentication on the simulator
+ * 
+ * Uses Touch ID ('fingerTouch') and FaceID ('pearl') to perform
+ * a successful authentication.
+ */
 + (void)successfulAuthentication;
+
+/*!
+ * Performs an unsuccessful biometric authentication on the simulator
+ * 
+ * Uses Touch ID ('fingerTouch') and FaceID ('pearl') to perform
+ * an unsuccessful authentication.
+ */
 + (void)unsuccessfulAuthentication;
 
 @end
