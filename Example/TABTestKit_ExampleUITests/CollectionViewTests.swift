2e244144c1a54fdf11a48c8c5271f951002c134b
Kane Cheshire <kane.cheshire@theappbusiness.com>
2019-10-14 10:37:45 +0100

    Asserts correct states

--- Example/TABTestKit_ExampleUITests/CollectionViewTests.swift
+++ Example/TABTestKit_ExampleUITests/CollectionViewTests.swift
@@ -31,7 +31,8 @@ final class CollectionViewTests: TABTestCase, SystemPreferencesContext {
     Scenario("Scrolling until the last cell is hidden") {
       Given(the: state(of: collectionViewScreen.lastCell, is: .visible))
       When(I: scroll(collectionViewScreen, .from(.middle, to: .bottom), until: collectionViewScreen.lastCell, isNot: .exists))
-      Then(the: state(of: collectionViewScreen.firstCell, is: .exists, .visibleIn(collectionViewScreen.trait)))
+      Then(the: state(of: collectionViewScreen.firstCell, isNot: .exists))
+      And(the: state(of: collectionViewScreen.lastCell, isNot: .exists))
     }
   }
   
