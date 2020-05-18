80757581766186bac81647609fcfb592fbd58a6a
Kane Cheshire <kane.cheshire@theappbusiness.com>
2020-05-18 14:02:08 +0100

    Updated `open` func so that it automatically accepts permissions if needed

--- Example/TABTestKit_ExampleUITests/DeepLinkingTests.swift
+++ Example/TABTestKit_ExampleUITests/DeepLinkingTests.swift
@@ -10,31 +10,28 @@ import TABTestKit
 
 final class DeepLinkingTests: TABTestCase {
 
-  func test() {
-    Scenario("Deep linking from current state") {
-      Given(I: complete(biometricLoginScreen))
-      When(I: tap(tabBarScreen.otherTabBarButton))
-      And(I: open(.deepLinkURL))
-      And(I: tap("Open", in: openAppConfirmationAlert)) // You will need to erase the simulator if you've already accepted this alert once
-      Then(I: see(otherElementsScreen))
+    func test() {
+        Scenario("Deep linking from current state") {
+            Given(I: complete(biometricLoginScreen))
+            When(I: tap(tabBarScreen.otherTabBarButton))
+            And(I: open(.deepLinkURL))
+            Then(I: see(otherElementsScreen))
+        }
+        Scenario("Deep linking from terminated state") {
+            Given(I: see(otherElementsScreen))
+            When(I: terminateTheApp)
+            And(I: open(.deepLinkURL))
+            And(I: waitForApp(toBe: .runningForeground))
+            Then(I: doNotSee(otherElementsScreen))
+            And(I: see(biometricLoginScreen))
+        }
     }
-    Scenario("Deep linking from terminated state") {
-      Given(I: see(otherElementsScreen))
-      When(I: terminateTheApp)
-      And(I: open(.deepLinkURL))
-      And(I: waitForApp(toBe: .runningForeground))
-      Then(I: doNotSee(otherElementsScreen))
-      And(I: see(biometricLoginScreen))
-    }
-  }
 
 }
 
 
 extension URL {
 
-  static let deepLinkURL = URL(string: "my-amazing-app://deep-link")!
+    static let deepLinkURL = URL(string: "my-amazing-app://deep-link")!
 
 }
-
-let openAppConfirmationAlert = Alert(id: "“TABTestKit_ExampleUITests-Runner” wants to open “TABTestKit_Example”", parent: springboard)
--- TABTestKit/Classes/Elements/Icon.swift
+++ TABTestKit/Classes/Elements/Icon.swift
@@ -10,28 +10,28 @@ import XCTest
 /// Represents an icon, like an app icon on the Home Screen.
 public struct Icon: Element, Tappable {
 
-  public let id: String?
-  public let index: Int
-  public let parent: Element
-  public let type: XCUIElement.ElementType = .icon
+    public let id: String?
+    public let index: Int
+    public let parent: Element
+    public let type: XCUIElement.ElementType = .icon
 
-  init(id: String? = nil, index: Int = 0, parent: Element = App.shared) {
-    self.id = id
-    self.index = index
-    self.parent = parent
-  }
+    init(id: String? = nil, index: Int = 0, parent: Element = App.shared) {
+        self.id = id
+        self.index = index
+        self.parent = parent
+    }
 
 }
 
 public extension Icon {
 
-  /// Represents the icon on the home screen for the UI test runner.
-  static let testRunner = Icon(id: Bundle.main.appName, parent: springboard)
+    /// Represents the icon on the home screen for the UI test runner.
+    static let testRunner = Icon(id: Bundle.main.appName, parent: springboard)
 
 }
 
-private extension Bundle {
+extension Bundle {
 
-  var appName: String { return infoDictionary?["CFBundleName"] as? String ?? "" }
+    var appName: String { return infoDictionary?["CFBundleName"] as? String ?? "" }
 
 }
