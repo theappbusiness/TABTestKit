03718c84ed59b4af0be1ab79aafb0ae765cd3db4
Anna Piktas <annapiktas@theappbusiness.com>
2020-07-23 18:11:59 +0200

    Temp fix: increasing the timeout to 3min for .runningBackgroundSuspended for iOS 12

--- TABTestKit/Classes/Apps/BaseApp.swift
+++ TABTestKit/Classes/Apps/BaseApp.swift
@@ -32,7 +32,7 @@ open class BaseApp: XCUIApplication {
 		if #available(iOS 13.0, *) { // https://github.com/theappbusiness/TABTestKit/issues/67
 			XCTAssertTrue(wait(for: .runningBackground, timeout: 10), "Failed waiting for app to become .runningBackground")
 		} else {
-			XCTAssertTrue(wait(for: .runningBackgroundSuspended, timeout: 30), "Failed waiting for app to become .runningBackgroundSuspended")
+			XCTAssertTrue(wait(for: .runningBackgroundSuspended, timeout: 180), "Failed waiting for app to become .runningBackgroundSuspended")
 		}
 	}
 	
