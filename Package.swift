fae3b11ebb5beeab4005fc078d1c6941446c7aa3
Kane Cheshire <kane.cheshire@googlemail.com>
2020-08-04 10:48:19 +0100

    Fixed package file

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let supportedPlatforms: [SupportedPlatform] = [.iOS(.v12)]
 let biometricsTarget: Target = .target(name: "Biometrics", path: "TABTestKit/Classes/Biometrics/")
 
 /// The main TTK target. this has a dependency on the biometrics target. Having two targets means we can have a mixed-language package, which is otherwise not supported.
-let mainTarget: Target = .target(name: name, dependencies: [.target(name: biometricsTarget.name)], path: "TABTestKit/Classes/", exclude: ["Biometric/"])
+let mainTarget: Target = .target(name: name, dependencies: [.target(name: biometricsTarget.name)], path: "TABTestKit/Classes/", exclude: ["Biometrics/"])
 
 /// The final product, using all of the above.
 let product: Product = .library(name: name, type: .dynamic, targets: [mainTarget.name])
